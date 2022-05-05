1dc966ee2b684ec0551e08c39b00fd81ae791bd5
Zac Sweers <pandanomic@gmail.com>
2022-05-05 10:24:37 -0400

    Raise limit to 100 + fix image URL

--- socialtees-custom.js
+++ socialtees-custom.js
@@ -4,7 +4,7 @@
       $document: null,
       $preFooter: null,
       $footer: null,
-  
+
       pets: null,
       dogs: [],
       cats: [],
@@ -14,7 +14,7 @@
       $petsFilters: null,
       $filterBtns: null,
       $loader: null,
-  
+
       isLoading: true,
       // PetFinder only shows 20 per page and we don't currently support pagination
       numToLoad: 20,
@@ -22,31 +22,31 @@
       petsVisible: 0,
       petsToShow: 0,
       filter: null,
-  
+
       loaderURL:
         "https://static1.squarespace.com/static/572b597a1bbee0f4e8d01e5e/t/5766ebc23e00be24e54c7b08/1466362818303/spinner.gif",
-  
+
       init: function () {
         st.$win = $(window);
         st.$document = $(document);
         st.$preFooter = $("#prefooter");
         st.$footer = $("#footer");
         st.$petsWrapper = $("#js-petfinder");
-  
+
         if (st.$petsWrapper.length > 0) {
           // Add loader to page.
           st.$loader = $(st.getLoaderDOM());
           st.$petsWrapper.append(st.$loader);
-  
+
           st.loadPets();
-  
+
           st.$win.scroll(st.checkScroll);
         }
       },
-  
+
       checkScroll: function () {
         var footerHeight = st.$preFooter.outerHeight() + st.$footer.outerHeight();
-  
+
         if (st.petsVisible >= st.petsToShow || st.isLoading) {
           return false;
         } else if (
@@ -58,15 +58,15 @@
           st.addPets();
         }
       },
-  
+
       loadPets: function () {
         const pf = new petfinder.Client({
           apiKey: "eWWxBarOWYLSKqUEjBKb6B8k04QSaEdJz6WHhSeSQqNj35LC23",
           secret: "dQp8i1PIMXv49VABfqV8iNc9bwWtq9AEPa8nQZeN",
         });
-  
+
         pf.animal
-          .search({ organization: "NY835" })
+          .search({ organization: "NY835", limit: 100 })
           .then((res) => {
             st.pets = res.data.animals;
             st.totalPets = st.petsToShow = st.pets.length;
@@ -82,10 +82,10 @@
             st.$petsWrapper.append(st.getErrorDOM());
           });
       },
-  
+
       processPets: function () {
         if (st.pets === null) return;
-  
+
         var i = 0;
         for (i; i < st.pets.length; i++) {
           if (st.pets[i].type === "Dog") {
@@ -97,10 +97,10 @@
           }
         }
       },
-  
+
       addPets: function () {
         st.$petsContent.append(st.getPetListingsDOM());
-  
+
         window.setTimeout(function () {
           if (!st.$petsFilters.hasClass("loaded")) {
             st.$petsFilters.addClass("loaded");
@@ -110,20 +110,20 @@
           st.isLoading = false;
         }, 250);
       },
-  
+
       getPetListingsDOM: function () {
         var petListings;
         var i = st.totalPets - st.petsVisible - 1;
         var threshold;
-  
+
         if (st.filter === null) {
           threshold = i - st.numToLoad < 0 ? -1 : i - st.numToLoad;
         } else {
           threshold = -1;
         }
-  
+
         petListings = "<div class='row sqs-row'>";
-  
+
         for (i; i > threshold; i--) {
           if (st.filter === null || st.filter === st.pets[i].type) {
             petListings += st.getPetDOM(i);
@@ -132,27 +132,26 @@
               petListings += st.getPetDOM(i);
             }
           }
-  
+
           st.petsVisible += 1;
         }
-  
+
         petListings += "</div>";
-  
+
         return petListings;
       },
-  
+
       getPetDOM: function (i) {
         var petDOM;
         var imgURL;
         var size = st.pets[i].size;
         var gender = st.pets[i].gender;
-  
+
         // NOTE: Normally we'd use https://photos.petfinder.com/ as the prefix, but their cert
         // is invalid and fails to load at https. So instead, we bypass it and load cloudfront
         // directly.
         // https://github.com/petfinder-com/petfinder-js-sdk/issues/22
-        imgURL =
-          "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/" + st.pets[i].id + "/1/";
+        imgURL = st.pets[i].photos[0].large;
         petDOM = "<div class='col sqs-col-4 span-4'>";
         petDOM += "<div class='sqs-block image-block html-block'>";
         petDOM += "<div class='petfinder__img-wrapper'>";
@@ -164,7 +163,7 @@
         petDOM += "<img alt='" + st.pets[i].name + "' src='" + imgURL + "' />"
         petDOM += "</div>";
         petDOM += "<h3>" + st.pets[i].name + "</h3>";
-  
+
         petDOM += "<ul>";
         if (st.pets[i].breeds.primary) {
           petDOM += "<li>" + st.pets[i].breeds.primary;
@@ -185,7 +184,7 @@
         st.pets[i].id +
         "' target='_blank'>Full bio on petfinder</a></li>";
         petDOM += "</ul>";
-  
+
         if (st.pets[i].type === "Dog") {
           petDOM +=
             "<a href='/application' title='apply to adopt this pet'>Apply to adopt " + st.pets[i].name + "!</a>";
@@ -198,19 +197,19 @@
         }
         petDOM += "</div>";
         petDOM += "</div>";
-  
+
         return petDOM;
       },
-  
+
       setupDOM: function () {
         var filter =
           "<div id='js-petfinder__filters' class='sqs-block button-block sqs-block-button'></div>";
         var content = "<div id='js-petfinder__content'></div>";
-  
+
         st.$petsWrapper.append(filter);
         st.$petsWrapper.append(content);
       },
-  
+
       addFilters: function () {
         var filters = "<h3>Filter by:</h3>";
         filters +=
@@ -222,32 +221,32 @@
         // "Other" isn't supported right now
         // filters +=
         //  "<button class='sqs-block-button-element--small' data-animal='Other'>other</button>";
-  
+
         st.$petsFilters.append(filters);
-  
+
         st.bindFilterClicks();
       },
-  
+
       bindFilterClicks: function () {
         st.filterBtns = st.$petsFilters.children("button");
-  
+
         st.filterBtns.on("click", function (e) {
           var $target = $(e.target);
           st.filterBtns.removeClass("active");
-  
+
           st.filter = $target.data("animal");
           $target.addClass("active");
-  
+
           st.startFilters();
         });
       },
-  
+
       startFilters: function () {
         st.$petsContent.find(".col").addClass("loaded");
         st.$petsContent.html(st.$loader);
-  
+
         st.petsVisible = 0;
-  
+
         if (st.filter === "Dog") {
           st.petsToShow = st.dogs.length;
         } else if (st.filter === "Cat") {
@@ -257,30 +256,29 @@
         } else {
           st.petsToShow = st.pets.length;
         }
-  
+
         st.addPets();
       },
-  
+
       getErrorDOM: function () {
         var msg = "<h1>Looks like there was an error</h1>";
         msg +=
           "<p>See our adoptable pets <a href='https://www.petfinder.com/pet-search?shelterid=NY835' title='Social tees on Petfinder'>here.</a></p>";
-  
+
         return msg;
       },
-  
+
       getLoaderDOM: function () {
         var loader = "<div class='loader__wrapper'>";
         loader +=
           "<img src='" + st.loaderURL + "' alt='loader' class='loader' />";
         loader += "</div>";
-  
+
         return loader;
       },
     };
-  
+
     $(function () {
       st.init();
     });
   })();
-  
